b1a1d6266a4bd9a1e61c69b4eaa8ff5281d50758
M0nstr1kUwU <237014391+M0nstr1kUwU@users.noreply.github.com>
2026-03-19 12:44:18 +0300

    Update ClassWork

--- nodes/app.js
+++ nodes/app.js
@@ -11,7 +11,24 @@ async function loadNotes(){
     }
     catch(error){
         console.log("Ошибка", error);
-        stats.innerHTML = `Информации о заметках нет`
+        stats.innerHTML = `Информации о заметках нет`;
+    }
+}
+
+
+async function addNote(){
+    const title = prompt("Введите название ");
+    const content = prompt("Введите содержание ");
+    
+    try{
+        await fetch('/api/notes', {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({title, content})
+        });
+    }
+    catch(error){
+        console.log("Ошибка", error);
     }
 }
 
--- nodes/server.js
+++ nodes/server.js
@@ -8,7 +8,7 @@ const fileManager = require("./utils/fileManager");
 let notes = fileManager.loadFile();
 
 const server = http.createServer(async (req, res) => {
-    const {url, method} = req
+    const {url, method} = req;
 
     // Root Routers
 
@@ -16,22 +16,45 @@ const server = http.createServer(async (req, res) => {
         const html = await fs.readFile(path.join(__dirname, 'index.html'), 'utf-8');
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(html);
+        return;
     }
 
     if(url === '/app.js' && method === 'GET'){
         const js = await fs.readFile(path.join(__dirname, 'app.js'), 'utf-8');
-        res.writeHead(200, {'Content-Type': 'application/javascript'})
+        res.writeHead(200, {'Content-Type': 'application/javascript'});
         res.end(js);
+        return;
     }
 
     // Api Routers
 
-    if(url === 'api/notes' && method === 'GET'){
+    if(url === '/api/notes' && method === 'GET'){
         res.writeHead(200, {'Content-Type': 'application/json'});
         res.end(JSON.stringify(notes))
+        return;
+    }
+
+    if(url === '/api/notes' && method === 'POST'){
+        let body = "";
+        req.on('data', (chunk) => body += chunk);
+        req.on('end', async() => {
+            const {title, content} = JSON.parse(body);
+            const newNote = {
+                id: notes.length + 1,
+                title: title,
+                content: content,
+                date: new Date().toLocaleString(),
+            };
+            notes.push(newNote);
+            fileManager.saveFile(notes);
+            console.log(`Заметка ${newNote.title} сохранена!`);
+            });
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({success: true}));
+        return;
     }
 });
 
 server.listen(3000, () => {
-    console.log("Сервер запущен на порту http://localhost:3000")
+    console.log("Сервер запущен на порту http://localhost:3000"); 
 });
