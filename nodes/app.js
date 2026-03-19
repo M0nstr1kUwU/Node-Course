ebce96d7b741000b7724bbcbce99427926f52ad9
M0nstr1kUwU <237014391+M0nstr1kUwU@users.noreply.github.com>
2026-03-19 10:41:42 +0300

    Update

---
+++ nodes/app.js
@@ -0,0 +1,18 @@
+const stats = document.getElementById('stats');
+const content = document.getElementById('content');
+
+let notes = []
+
+async function loadNotes(){
+    try{
+        const res = await fetch('/api/notes');
+        notes = await res.json();
+        stats.innerText = 'Заметок ${notes.length}';
+    }
+    catch(error){
+        console.log("Ошибка", error);
+        stats.innerHTML = `Информации о заметках нет`
+    }
+}
+
+loadNotes();
--- nodes/server.js
+++ nodes/server.js
@@ -9,6 +9,27 @@ let notes = fileManager.loadFile();
 
 const server = http.createServer(async (requestAnimationFrame, res) => {
     const {url, method} = req
+
+    // Root Routers
+
+    if(url === '/' && method === 'GET'){
+        const html = await fs.readFile(path.join(__dirname, 'index.html'), 'utf-8');
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(html);
+    }
+
+    if(url === '/app.js' && method === 'GET'){
+        const js = await fs.readFile(path.join(__dirname, 'app.js'), 'utf-8');
+        res.writeHead(200, {'Content-Type': 'application/javascript'})
+        res.end(js);
+    }
+
+    // Api Routers
+
+    if(url === 'api/notes' && method === 'GET'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(notes))
+    }
 });
 
 server.listen(3000, () => {
