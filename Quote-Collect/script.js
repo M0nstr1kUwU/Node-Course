116c12aeb750da392fabbd548f3467a2d615a1ec
M0nstr1k <237014391+M0nstr1kUwU@users.noreply.github.com>
2026-04-01 08:59:15 +0300

    Update

--- Quote-Collect/script.js
+++ Quote-Collect/script.js
@@ -1,7 +1,7 @@
-const quotes = [];
+let quotes = [];
 
 function saveQuotes(){
-    localStorage.setItem('quotes', quotes);
+    localStorage.setItem('quotes', JSON.stringify(quotes));
 }
 
 function loadQuotes(){
@@ -17,14 +17,23 @@ function renderQuotes(){
 
     quotes.forEach((quote, index) => {
         html += `
-            <div class=""> ${quote.textInput}</div>
-            <div class=""> ${quote.authorInput}</div>
-            <button class="delete-btn" data-index="${index}"> Удалить </button>
+            <div class="quote-card">
+                <div class="quote-text"> ${quote.textInput}</div>
+                <div class="quote-author"> ${quote.authorInput}</div>
+                <button class="delete-btn" data-index="${index}"> Удалить </button>
+            </div>
         `;
     });
     list.innerHTML = html;
 
-    document.querySelectorAll("delete-btn").forEach(btn => { btn.addEventListener('click', deleteQuote); });
+    document.querySelectorAll(".delete-btn").forEach(btn => { 
+        btn.addEventListener('click', (e) => {
+            const index = parseInt(e.target.dataset.index);
+            quotes.splice(index, 1);
+            saveQuotes();
+            renderQuotes();
+        }); 
+    });
 }
 
 function addQuote(){
@@ -32,19 +41,21 @@ function addQuote(){
     const author = document.getElementById('quoteAuthor');
 
     const textInput = text.value;
-    const authorInput = text.value;
+    const authorInput = author.value;
 
     console.log(textInput);
     console.log(authorInput);
 
     quotes.push({textInput, authorInput})
-}
+    saveQuotes();
+    renderQuotes();
 
-function deleteQuote(){
-
+    text.value = '';
+    author.value = '';
 }
 
 document.getElementById('addBtn').addEventListener('click', addQuote);
 
 
 loadQuotes();
+renderQuotes();
