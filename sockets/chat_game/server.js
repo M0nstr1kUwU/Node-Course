7f05a89f44452615c1c59351d220e4e83e5aaa9a
M0nstr1kUwU <237014391+M0nstr1kUwU@users.noreply.github.com>
2026-06-10 09:33:22 +0300

    Update

--- sockets/chat_game/server.js
+++ sockets/chat_game/server.js
@@ -1,19 +1,48 @@
-import fastify from 'fastify';
+import Fastify from 'fastify';
 import fastifyStatic from '@fastify/static';
+import { Server } from 'socketio';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import path from 'path';
+import { Socket } from 'dgram';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const PORT = 8001;
-const fastify_server = fastify({logger: true});
+const fastify_server = Fastify({logger: true});
 
 fastify_server.register(fastifyStatic, {root: __dirname});
-
-fastify.get('/', (_, rep) => {
-    rep.sendFile('index.html');
+fastify_server.get('/', (request, reply) => {
+    reply.sendFile('index.html');
 });
 
-fastify.listen({port: PORT, host: '0.0.0.0'}, () => {
+fastify_server.listen({port: PORT, host: '0.0.0.0'}, () => {
     console.log(`Server run: http://localhost:${PORT}`);
+});
+
+const io = new Server(fastify_server.server);
+const sessions = new Map();
+
+class PlayerData{
+    constructor(playerId, playerName, selectModel){
+        this.playerId = playerId;
+        this.playerName = playerName;
+        this.currentModel = selectModel;
+        this.position = {x: 0, y: 0, z: 0};
+        this.rotation = {x: 0, y: 0, z: 0};
+    }
+}
+
+io.on('connection', (socket) => {
+    io.on('join', (sessionId, playerName) => {
+        if (!sessions.has(sessionId)) {
+            sessions.set(sessionId, new Map());
+        }
+        else{
+            const session = new Map();
+            sessions.get(sessionId);
+            const playerData = new PlayerData(socket.id, sessions.size, 0);
+            session.set(sessionId, playerData);
+            socket.broadcast.emit('playerJoin', playerData);
+        }
+        
+    });
 });
